1f79ab072ff75b1a9a836dc0b5ddf4e92b9ecc82
LumiereJE <105902395+LumiereJE@users.noreply.github.com>
2022-05-23 14:46:30 +0900

    1.0.1

--- Granhand/javascript/granhand_web_store.js
+++ Granhand/javascript/granhand_web_store.js
@@ -4,6 +4,26 @@ $(document).ready(function(){
     let sec_top = $('main').offset().top;
 
 
+    let ns_st = [];
+    for(let i=0; i<$('.ns_st').length; i++) {
+        ns_st[i] = $('.ns_st').eq(i).offset().top;
+    }  
+    let mp_st = [];
+    for(let i=0; i<$('.mp_st').length; i++) {
+        mp_st[i] = $('.mp_st').eq(i).offset().top;
+    }  
+    let sc_st = [];
+    for(let i=0; i<$('.sc_st').length; i++) {
+        sc_st[i] = $('.sc_st').eq(i).offset().top;
+    }  
+    let sg_st = [];
+    for(let i=0; i<$('.sg_st').length; i++) {
+        sg_st[i] = $('.sg_st').eq(i).offset().top;
+    }  
+    let bc_st = [];
+    for(let i=0; i<$('.bc_st').length; i++) {
+        bc_st[i] = $('.bc_st').eq(i).offset().top;
+    }  
 
     $(window).scroll(function(){
 
@@ -19,8 +39,47 @@ $(document).ready(function(){
             $('header').removeClass('header_active')
         }
 
+////////////////////////////////////////////////////
 
+        for(let i=0; i<$('.ns_st').length; i++) {
+            if($('.ns_st').eq(i).hasClass('sec_active') == false) {
+                if(s_top + 400 >= ns_st[i]) {
+                    $('.ns_st').eq(i).addClass('sec_active')
+                }
+            }    
+        }
 
+        for(let i=0; i<$('.mp_st').length; i++) {
+            if($('.mp_st').eq(i).hasClass('sec_active') == false) {
+                if(s_top + 400 >= mp_st[i]) {
+                    $('.mp_st').eq(i).addClass('sec_active')
+                }
+            }    
+        }
+
+        for(let i=0; i<$('.sc_st').length; i++) {
+            if($('.sc_st').eq(i).hasClass('sec_active') == false) {
+                if(s_top + 400 >= sc_st[i]) {
+                    $('.sc_st').eq(i).addClass('sec_active')
+                }
+            }    
+        }
+
+        for(let i=0; i<$('.sg_st').length; i++) {
+            if($('.sg_st').eq(i).hasClass('sec_active') == false) {
+                if(s_top + 400 >= sg_st[i]) {
+                    $('.sg_st').eq(i).addClass('sec_active')
+                }
+            }    
+        }
+
+        for(let i=0; i<$('.bc_st').length; i++) {
+            if($('.bc_st').eq(i).hasClass('sec_active') == false) {
+                if(s_top + 400 >= bc_st[i]) {
+                    $('.bc_st').eq(i).addClass('sec_active')
+                }
+            }    
+        }
 
 
 
